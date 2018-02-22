f99440b833eb820ee297e3fd559864c5ced48d85
kss003 <kylesmith0630@gmail.com>
2018-02-22 10:23:11 -0600

    Add WeaterReminderIntentService class

--- app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -4,17 +4,17 @@ import android.content.Context;
 
 import com.example.android.background.utilities.PreferenceUtilities;
 
-class ReminderTasks {
+public class ReminderTasks {
 
     public static String ACTION_INCREMENT_WATER_COUNT;
 
-    public static void executask(Context context, String action) {
-        if (action == ACTION_INCREMENT_WATER_COUNT) {
-            increamentWaterCount(context);
+    public static void executeTask(Context context, String action) {
+        if (action.equals(ACTION_INCREMENT_WATER_COUNT)) {
+            incrementWaterCount(context);
         }
     }
 
-    private static void increamentWaterCount(Context context) {
+    private static void incrementWaterCount(Context context) {
         PreferenceUtilities.incrementWaterCount(context);
     }
 }
