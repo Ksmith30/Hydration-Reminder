eb3a8fbbba0c6dcad57fd2c12e0c9f7c2ab23e71
kss003 <kylesmith0630@gmail.com>
2018-02-26 09:17:48 -0600

    Fix Null String

--- app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -6,7 +6,7 @@ import com.example.android.background.utilities.PreferenceUtilities;
 
 public class ReminderTasks {
 
-    public static String ACTION_INCREMENT_WATER_COUNT;
+    public static final String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
 
     public static void executeTask(Context context, String action) {
         if (action.equals(ACTION_INCREMENT_WATER_COUNT)) {
