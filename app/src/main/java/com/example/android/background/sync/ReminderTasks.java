1175a08b636c890e5e0facbdb1bf87a66c52fe3a
kss003 <kylesmith0630@gmail.com>
2018-02-26 13:40:49 -0600

    Add dismiss action

--- app/src/main/java/com/example/android/background/sync/ReminderTasks.java
+++ app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -2,19 +2,27 @@ package com.example.android.background.sync;
 
 import android.content.Context;
 
+import com.example.android.background.utilities.NotificationUtils;
 import com.example.android.background.utilities.PreferenceUtilities;
 
 public class ReminderTasks {
 
     public static final String ACTION_INCREMENT_WATER_COUNT = "increment-water-count";
+    public static final String ACTION_DISMISS_NOTIFICATION = "dismiss-notification";
 
     public static void executeTask(Context context, String action) {
         if (action.equals(ACTION_INCREMENT_WATER_COUNT)) {
             incrementWaterCount(context);
+        } else if (action.equals(ACTION_DISMISS_NOTIFICATION)) {
+            dismissNotification(context);
         }
     }
 
     private static void incrementWaterCount(Context context) {
         PreferenceUtilities.incrementWaterCount(context);
+    }
+
+    private static void dismissNotification(Context context) {
+        NotificationUtils.clearAllNotifications(context);
     }
 }
--- app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
+++ app/src/main/java/com/example/android/background/utilities/NotificationUtils.java
@@ -1,6 +1,7 @@
 package com.example.android.background.utilities;
 
 import android.annotation.SuppressLint;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -19,31 +20,16 @@ import com.example.android.background.R;
 
 public class NotificationUtils {
 
-    public static final int NOTIFICATION_REQUEST = 1;
-    public static final String CHANNEL_ID = "channel";
+    private static final int NOTIFICATION_REQUEST = 1;
+    private static final String CHANNEL_ID = "channel";
 
-    // TODO (7) Create a method called remindUserBecauseCharging which takes a Context.
-    // This method will create a notification for charging. It might be helpful
-    // to take a look at this guide to see an example of what the code in this method will look like:
-    // https://developer.android.com/training/notify-user/build-notification.html
-    // TODO (8) Get the NotificationManager using context.getSystemService
-    // TODO (9) Create a notification channel for Android O devices
-    // TODO (10) In the remindUser method use NotificationCompat.Builder to create a notification
-    // that:
-    // - has a color of R.colorPrimary - use ContextCompat.getColor to get a compatible color
-    // - has ic_drink_notification as the small icon
-    // - uses icon returned by the largeIcon helper method as the large icon
-    // - sets the title to the charging_reminder_notification_title String resource
-    // - sets the text to the charging_reminder_notification_body String resource
-    // - sets the style to NotificationCompat.BigTextStyle().bigText(text)
-    // - sets the notification defaults to vibrate
-    // - uses the content intent returned by the contentIntent helper method for the contentIntent
-    // - automatically cancels the notification when the notification is clicked
-    // TODO (11) If the build version is greater than JELLY_BEAN and lower than OREO,
-    // set the notification's priority to PRIORITY_HIGH.
-    // TODO (12) Trigger the notification by calling notify on the NotificationManager.
-    // Pass in a unique ID of your choosing for the notification and notificationBuilder.build()
-    public void remindUserBecauseCharging(Context context) {
+    public static void clearAllNotifications(Context context) {
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancelAll();
+    }
+
+    public static void remindUserBecauseCharging(Context context) {
 
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
@@ -68,6 +54,7 @@ public class NotificationUtils {
                         .bigText("Hello"))
                 .setContentIntent(contentIntent(context))
                 .setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_VIBRATE)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN && Build.VERSION.SDK_INT <=
@@ -78,14 +65,14 @@ public class NotificationUtils {
         notificationManager.notify(NOTIFICATION_REQUEST, mBuilder.build());
     }
 
-    public PendingIntent contentIntent(Context context) {
+    public static PendingIntent contentIntent(Context context) {
         Intent mainActivityIntent = new Intent(context, MainActivity.class);
         return PendingIntent.getActivity(context, NOTIFICATION_REQUEST,
                 mainActivityIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
     }
 
-    public Bitmap largeIcon(Context context) {
+    public static Bitmap largeIcon(Context context) {
         Resources resources = context.getResources();
         return BitmapFactory.decodeResource(resources, R.drawable.ic_local_drink_black_24px);
     }
